27a29c871d1fcf5cdc813fa909516fd3f0ff455b
aljunggren <aljunggren@allegient.com>
2022-01-22 10:18:08 -0500

    Package name update

--- src/test/java/io/ljunggren/dateUtils/DateUtilsTest.java
+++ src/test/java/io/ljunggren/dateUtils/DateUtilsTest.java
@@ -1,6 +1,8 @@
-package com.ljunggren.dateUtils;
+package io.ljunggren.dateUtils;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.text.ParseException;
 import java.util.Date;
